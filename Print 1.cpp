83887ac7d4bf984bc186048c61142a7217464b46
Suraj Doss <suraj_sebastien@yahoo.com>
2021-03-02 21:06:54 +0530

    Add files via upload

---
+++ Print 1.cpp
@@ -0,0 +1,23 @@
+/*
+Matrix pattern Printing
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n, m;
+    cout<<"Size of Matrix N M :";
+    cin>>n>>m;
+
+    for(int i = 0; i< n; i++){
+        for (int j= 0; j<m; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
