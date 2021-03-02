563a97bbba798c3d0446cb100edcb110d30b70f9
Suraj Doss <suraj_sebastien@yahoo.com>
2021-03-02 21:15:55 +0530

    Add files via upload

---
+++ print 2.cpp
@@ -0,0 +1,21 @@
+/*
+Pyramid Printing 1
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cout <<"Size of matrix as Len:";
+    cin>>n;
+
+    for (int i =0; i<=n; i++){
+        for(int j=0; j<i; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
